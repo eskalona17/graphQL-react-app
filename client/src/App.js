5cce5f952163963ff11dfd02d9632de77b8a2be2
eskalona17 <eskalona_17@hotmail.com>
2020-05-12 16:45:32 +0200

    connecting frontend with backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import MessageList from './components/MessageList'
+import MessageForm from './components/MessageForm'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MessageList} />
+        <Route exact path="/new-message" component={MessageForm} />
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3001",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
